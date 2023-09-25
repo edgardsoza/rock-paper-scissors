99d0ee80d70cdd1e3acad7d2883cb118090aac45
Amr hany El mahdy <amrhany1214@gmail.com>
2023-09-25 23:51:23 +0300

    Using JS Functions

--- main.js
+++ main.js
@@ -1,51 +1,73 @@
-const options = ['rock', 'paper' , 'scissors']
+const options = ['rock', 'paper', 'scissors'];
+let humanCounter = 0;
+let machineCounter = 0;
+let roundNumber = 0;
 
-function computerPlay () {
-    return options[Math.floor(Math.random()*options.length)]
+function computerPlay() {
+    return options[Math.floor(Math.random() * options.length)];
 }
 
-function playRound () {
-    let playerSelection = (prompt('Enter your input, you miserable Human')).toLowerCase();
+function playRound(playerSelection) {
     let computerSelection = computerPlay();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    var playerImg = "images/" + playerSelection + ".png";
+    var computerImg = "images/" + computerSelection + ".png";
+    document.querySelectorAll("img")[0].setAttribute("src", playerImg);
+    document.querySelectorAll("img")[1].setAttribute("src", computerImg);
     if (playerSelection == computerSelection) {
-        return 0
-    } else if (playerSelection == 'rock' & computerSelection == "scissors") {
-        return 1
-    } else if (playerSelection == 'rock' & computerSelection == "paper") {
-        return 2
-    } else if (playerSelection == 'paper' & computerSelection == "scissors") {
-        return 2
-    } else if (playerSelection == 'paper' & computerSelection == "rock") {
-        return 1
-    } else if (playerSelection == 'scissors' & computerSelection == "rock") {
-        return 2
-    } else if (playerSelection == 'scissors' & computerSelection == "paper") {
-        return 1
+        return 0;
+    } else if (
+        (playerSelection == 'rock' && computerSelection == "scissors") ||
+        (playerSelection == 'paper' && computerSelection == "rock") ||
+        (playerSelection == 'scissors' && computerSelection == "paper")
+    ) {
+        return 1; // Player wins
+    } else {
+        return 2; // Computer wins
     }
 }
 
-function game() {
-        let humanCounter = 0;
-        let machineCounter = 0;
-        let i = 0;
-        while (i<5) {
-            let round = playRound();
-            if (round == 1) {
-                humanCounter += 1;
-                i++;
-                console.log(`Humans won this round!! Humans=${humanCounter} and Machines=${machineCounter}`)
-            } else if (round == 2) {
-                machineCounter +=  1;
-                i++;
-                console.log(`The AI has won this one hahahaha!! Machines=${machineCounter} and Humans=${humanCounter}`)
-            } else {
-                console.log(`This round goes for nobody!! Try again you human!! Machines=${machineCounter} and Humans=${humanCounter}`)
-                continue;
-            }
-        }
-        if(machineCounter > humanCounter) {
-            return(`Machines have won!!! Your world is ours!! Final score is Machines=${machineCounter} and Humans=${humanCounter}`)
-        } else {
-            return(`Humans have won!!! We will send you our best antivirus!!  Final score is Humans=${humanCounter} and Machines=${machineCounter}`)
-        }
+function updateScore() {
+    document.querySelector("h1").style.fontSize = "3rem";
+    document.querySelector("h1").innerHTML = `Humans=${humanCounter} and Machines=${machineCounter} Round ${roundNumber}/5 FIGHT AGAIN HUMAN!`;
+}
+
+async function game() {
+    roundNumber++;
+    if (roundNumber > 5) {
+        endGame();
+        return;
     }
+
+    const activities = document.getElementById("choise");
+    let roundResult = playRound(activities.value);
+
+    if (roundResult == 1) {
+        humanCounter += 1;
+    } else if (roundResult == 2) {
+        machineCounter += 1;
+    }
+
+    updateScore();
+}
+
+function endGame() {
+    document.querySelector("h1").style.fontSize = "6rem";
+    if (machineCounter > humanCounter) {
+        document.querySelector("h1").innerHTML = "Machines have won!!! Your world is ours!! Final score is Machines=" + machineCounter + " and Humans=" + humanCounter +" REFRESH TO PLAY AGAIN";
+    } else {
+        document.querySelector("h1").innerHTML = "Humans have won!!! We will send you our best antivirus!!  Final score is Humans=" + humanCounter + " and Machines=" + machineCounter +" REFRESH TO PLAY AGAIN";
+    }
+    document.querySelector(".gameboard").style.display='none';
+    document.querySelector("select").style.display='none';
+    document.querySelector("label").style.display='none';
+
+}
+
+var activities = document.getElementById("choise");
+activities.addEventListener("change", function (e) {
+    game();
+});
+
+// Start the game
